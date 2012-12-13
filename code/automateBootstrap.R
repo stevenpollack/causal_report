c65e3e55d2d0ebd26e6516ad2f9502e38a272640
Steven Pollack <steven@pollackphoto.net>
2012-12-13 14:34:46 -0800

    changed automateBootstrap to work with boot::boot.

--- code/automateBootstrap.R
+++ code/automateBootstrap.R
@@ -1,7 +1,22 @@
+library(boot)
+
+# this function will be called by ran.gen in
+# boot::boot when performing "parametric"
+# bootstrap so it needs to take (d,p) as
+# arguments
+createBootstrapData <- function(data,weights) {
+  nrows <- nrow(data)
+  bootstrapIndices <- sample(x=1:nrows,size=nrows,
+                             replace=T,prob=weights)
+  return(data[bootstrapIndices,])
+}
+
+
 source("./code/bootstrap_all_fxn.R")
+load('../causal_data/Rdata/processed_aggregated_data.Rdata') # load data (uses steven's directory structure)
 
-B <- 1e3
-bootstrapResults <- sapply(X=c(FALSE,rep(TRUE,times=B-1)),
-                           FUN=calculateEstimates,dataframe=sdw)
+sdw <- processed.aggregated.data
+weights <- sdw[["IntWeight"]]/sum(sdw[["IntWeight"]])
 
-save(bootstrapResults,file="../causal_data/1000sampleBSrun.Rdata")
+B <- 3
+bootstrapResults <- boot(data=sdw, statistic=calculateEstimates, stype="w", R=500, weights=weights, parallel="multicore", ncpus=2)
